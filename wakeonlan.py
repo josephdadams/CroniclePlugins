16887807002fa2b797dcd110969065d97160125d
Joseph Adams <josephdadams@gmail.com>
2021-04-19 11:25:35 -0400

    add version info

--- wakeonlan.py
+++ wakeonlan.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+# File name: wakeonlan.py
+# Version: 1.0.0
+# Author: Joseph Adams
+# Email: josephdadams@gmail.com
+# Date created: 11/4/2020
+# Date last modified: 4/19/2021
+
+# Notes: Requires the 'wakeonlan' command line function to be installed.
+
 import sys
 import json
 import os
