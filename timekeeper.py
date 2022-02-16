0f5a39d5889b1d2b0916c02732b227e0a66eedd6
Joseph Adams <josephdadams@gmail.com>
2022-02-15 23:31:39 -0500

    add support for triggers

--- timekeeper.py
+++ timekeeper.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 # File name: timekeeper.py
-# Version: 1.0.0
+# Version: 1.0.1
 # Author: Joseph Adams
 # Email: josephdadams@gmail.com
 # Date created: 5/5/2021
-# Date last modified: 5/5/2021
+# Date last modified: 2/15/2022
 
 import sys
 import json
@@ -24,6 +24,19 @@ try:
 	label = data['params']['label']
 	roomID = data['params']['roomid']
 
+	triggers = ''
+
+	if 'triggers' in data['params']:
+		triggers = data['params']['triggers']
+	
+	if (triggers == ''):
+		triggers = '[]'
+
+	timerid = ''
+
+	if 'timerid' in data['params']:
+		timerid = data['params']['timerid']
+
 	url = 'http://' + ip + ':' + port + '/api/timer/add'
 
 	#create a new datetime object using the fields provided
@@ -34,7 +47,7 @@ try:
 		date_str = datetime.today().strftime('%m/%d/%y')
 
 	if (time_str == ''):
-		time_str = '06:00:00'
+		time_str = '6:00:00'
 
 	date_time_str = date_str + ' ' + time_str
 	d = datetime.strptime(date_time_str, '%m/%d/%y %H:%M:%S')
@@ -46,13 +59,18 @@ try:
 		"datetime": epochSeconds,
 		"publishMillis": 86400000, #1 day
 		"expireMillis": 60000, #1 minute
-		"roomID": roomID
+		"roomID": roomID,
+		"triggers": json.loads(triggers)
 	}
+
+	if (timerid != ''): #only include the ID if it was sent in the params
+		jsonData['id'] = timerid
 
 	print(jsonData)
 	
 	r = requests.post(url = url, json = jsonData)
 	
 	print('{ "complete": 1 }')
-except:
+except Exception as e:
+	print(e)
 	print('{ "complete": 1, "code": 999, "description": "Failed to execute." }')
