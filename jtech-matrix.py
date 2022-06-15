e9603b72d65612dd17b1432bee761e5b6d2461ee
Joseph Adams <josephdadams@gmail.com>
2022-06-15 11:33:20 -0400

    add jtech hdmi matrix plugin

---
+++ jtech-matrix.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# File name: jtech-matrix.py
+# Version: 1.0.0
+# Author: Joseph Adams
+# Email: josephdadams@gmail.com
+# Date created: 6/15/2022
+# Date last modified: 6/15/2022
+
+import sys
+import json
+import requests
+import time
+
+try:
+	print('Starting JTech HDMI Matrix Plugin.')
+
+	stdinput = sys.stdin.readline()
+	data = json.loads(stdinput)
+
+	ip = data['params']['ip']
+	output = data['params']['output']
+	input = data['params']['input']
+
+	cgiURL = '/videoset' + str(time.time())
+	payload = '#video_d out' + output + ' matrix=' + input
+
+	url = 'http://' + ip + cgiURL
+
+	print('Routing Output ' + output + ' to Input ' + input)
+	print('URL: ' + url)
+	print('Payload: ' + payload)
+	requests.post(url, data=payload, timeout=15)
+	
+	print('{ "complete": 1 }')
+except Exception as e:
+	print(e)
+	print('{ "complete": 1, "code": 999, "description": "Failed to execute." }')
